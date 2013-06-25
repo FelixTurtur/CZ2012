3112345c3d08eb9c56a8235e20ccb42a3f29ca88
Abbie James <onedayiwillfly@gmail.com>
2013-06-25 23:45:24 +0100

    Coding Notes, "Inconclusive" test

--- CleverZebra/LogixTests/LineTest.cs
+++ CleverZebra/LogixTests/LineTest.cs
@@ -9,8 +9,7 @@ namespace LogixTests
         public Line line;
 
         [TestMethod]
-        public void Create_And_Test_Line()
-        {
+        public void Create_And_Test_Line() {
             var line1 = new Line("B", 5);
             Assert.AreEqual("B", line1.identifier);
             object[] list = { 5, 10, 15, 20, 25 };
@@ -21,17 +20,25 @@ namespace LogixTests
 
         [TestInitialize]
         public void Initialize() {
-            line = new Line("A", 5);
         }
 
         [TestMethod]
         public void Test_AllButOneFound() {
+            line = new Line("A", 5);
             line.addRelation("A1", "B1");
             line.addRelation("A2", "B2");
             line.addRelation("A3", "B3");
             line.addRelation("A4", "B4");
             string AforB5 = line.checkForMatch("B5");
             Assert.AreEqual("A5", AforB5);
+        }
+
+        [TestMethod]
+        public void Test_InconclusiveResult() {
+            line = new Line("A", 5);
+            line.addRelation("A1", "B1");
+            string AforB = line.checkForMatch("B5");
+            Assert.IsNull(AforB);
         }
     }
 }
